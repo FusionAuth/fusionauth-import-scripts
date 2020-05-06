ecedeef037a7ce0350b242d4a01bc3786affbdb2
Trevor Smith <trevor@fusionauth.io>
2020-05-06 10:45:57 -0600

    Add CSV import script

--- README.md
+++ README.md
@@ -28,9 +28,39 @@ Finally, execute the Import script.
 ruby ./import.rb
 ```
 
+#### CSV
+
+The example [CSV import script](./csv/import.rb) provides a starting point for importing users from a CSV into FusionAuth.  This example maps the CSV fields by header from [the example CSV](./csv/example_users.csv) onto FusionAuth users within the `map_user` function.  You will likely need to update `map_user` to map the fields from your CSV accordingly.
+
+To begin, modify the section that begins with `BEGIN Modify these variables for your Import` with values that match your FusionAuth environment.
+
+The following gems are required to run this import script.
+
+```ruby
+require 'date'
+require 'csv'
+require 'fusionauth/fusionauth_client'
+```
+
+If you are familiar with Ruby you can optionally build a build file, or simply install these gems prior to running the script. The `date` gem is likely already available in your Ruby environment.
+
+```bash
+sudo gem install csv
+sudo gem install fusionauth_client
+```
+
+Finally, execute the Import script.
+
+```bash
+ruby ./import.rb
+```
+
 ### Project Layout
 
 ```
 auth0
 ├── import.rb
+csv
+├── import.rb
+├── example_users.csv
 ```
---
+++ csv/import.rb
@@ -0,0 +1,136 @@
+#!/usr/local/bin/ruby -w
+
+require 'csv'
+require 'date'
+require 'fusionauth/fusionauth_client'
+
+# BEGIN Modify these variables for your Import
+users_file = 'example_users.csv'
+
+$fusionauth_url = 'http://localhost:9011'
+$fusionauth_api_key = 'c716f996-1364-4ec3-b258-77c50d6f9a68'
+
+# You may import users with a registration to an application
+$fusionauth_application_id = '2abd1945-663f-4dcf-8d11-995f44d0b5b6'
+
+# Optionally specify the target tenant. If only one tenant exists this is optional and the users
+# will be imported to the default tenant. When more than one tenant exists in FusionAuth this is required.
+$fusionauth_tenant_id = '4715c371-2fcc-496c-b659-4af4f5bfde77'
+
+# END Modify these variables for your Import
+
+puts "FusionAuth Importer : CSV"
+puts " > User file: #{users_file}"
+
+# Map CSV header fields onto a FusionAuth user
+def map_user(csv_user)
+  user = {}
+
+  user['active'] = true
+  user['username'] = csv_user['LOGIN']
+  user['firstName'] = csv_user['FIRST_NAME']
+  user['middleName'] = csv_user['MIDDLE_NAME']
+  user['lastName'] = csv_user['LAST_NAME']
+  user['email'] = csv_user['EMAIL']
+
+  # Incoming format is '11-FEB-20 07.51.57.521000000 PM', convert to epoch milli
+  user['insertInstant'] = Date.parse(csv_user['REGISTRATION_DATE']).strftime("%Q")
+  user['lastLoginInstant'] = Date.parse(csv_user['LASTACTIVITY_DATE']).strftime("%Q")
+
+  # Register the user to an application
+  user['registrations'] = [{
+    'applicationId': $fusionauth_application_id,
+    'username': csv_user['LOGIN'],
+    'roles': ['user']
+  }]
+
+  # Import the password, with specified encryption scheme, factor and salt
+  user['encryptionScheme'] = 'salted-pbkdf2-hmac-sha256'
+  user['factor'] = 10_000
+  user['salt'] = csv_user['PASSWORD_SALT']
+  user['password'] = csv_user['PASSWORD']
+
+  return user
+end
+
+def import(users)
+  puts " > Call FusionAuth to import users"
+
+  import_request = {}
+  import_request['users'] = users
+  import_request['validateDbConstraints'] = false
+
+  # FusionAuth Import API
+  # https://fusionauth.io/docs/v1/tech/apis/users#import-users
+  client = FusionAuth::FusionAuthClient.new($fusionauth_api_key, $fusionauth_url)
+  client.set_tenant_id($fusionauth_tenant_id)
+  response = client.import_users(import_request)
+  if response.was_successful
+    puts " > Import success"
+  else
+    puts " > Import failed. Status code #{response.status}. Error response:\n #{response.error_response}"
+    exit 1
+  end
+end
+
+# Initialize a user array to hold chunks of 10k users before calling the Import API
+users = []
+emails = []
+user_names = []
+duplicate_emails = []
+duplicate_user_names = []
+
+count = 0
+duplicate_count = 0
+
+csv_users = CSV.parse(File.read(users_file), headers: true)
+# Iterate the rows of the CSV and map the fields onto a FusionAuth User
+csv_users.each { |csv_user|
+  u = map_user(csv_user)
+
+  unless u['email'].nil?
+    unless emails.include? u['email']
+      emails.push u['email']
+    else
+      duplicate_emails.push u['username']
+      duplicate = true
+    end
+  end
+
+  unless u['username'].nil?
+    unless user_names.include? u['username']
+      user_names.push u['username']
+    else
+      duplicate_user_names.push u['username']
+      duplicate = true
+    end
+  end
+
+  unless duplicate
+    count = count + 1
+    users.push u
+  else
+    duplicate_count = duplicate_count + 1
+  end
+
+  # In chunks of 10k, request a bulk insert
+  if count % 10_000 == 0
+    import(users)
+    users = []
+  end
+}
+
+# Complete the import for anything that did not make the 10k threshold
+if users.length > 0
+  import(users)
+  users = []
+end
+
+puts "Duplicate users #{duplicate_count}"
+unless duplicate_emails.size == 0
+  duplicate_emails.each { |email| puts " > #{email}" }
+end
+unless duplicate_user_names.size == 0
+  duplicate_user_names.each { |user_name| puts " > #{user_name}" }
+end
+puts "Import complete. #{count} users imported."
