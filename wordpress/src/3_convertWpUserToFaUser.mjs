39fea63b0d9df98aeda8cd9a5927aaefc8aab68d
RichardJECooke <RichardJECooke@pm.me>
2024-02-27 11:00:36 +0200

    finish data import

--- wordpress/src/3_convertWpUserToFaUser.mjs
+++ wordpress/src/3_convertWpUserToFaUser.mjs
@@ -1,7 +1,5 @@
 import { promises as fs } from 'fs';
 import * as fs from 'fs';
-import util from 'util';
-import {FusionAuthClient} from '@fusionauth/typescript-client';
 import parser from 'stream-json';
 import StreamArray from 'stream-json/streamers/StreamArray.js';
 import Chain from 'stream-chain';
@@ -9,6 +7,7 @@ import * as phpunserialize from 'phpunserialize';
 
 const inputFilename = 'users.json';
 const outputFilename = 'faUsers.json';
+const dataToIgnore = ['first_name', 'last_name', 'rich_editing', 'syntax_highlighting', 'comment_shortcuts', 'admin_color', 'use_ssl', 'show_admin_bar_front', 'wp_user_level', 'show_welcome_panel', 'session_tokens', 'wp_dashboard_quick_press_last_post_id', 'community-events-location'];
 
 processUsers();
 
@@ -24,17 +23,6 @@ async function processUsers() {
     await fs.appendFile(outputFilename, JSON.stringify(faUser), 'utf8');
   }
   await fs.appendFile(outputFilename, '\n]', 'utf8');
-}
-
-function isSerializedPhp(value) {
-  if (typeof value !== 'string')
-    return false;
-  const trimmedValue = value.trim();
-  return (
-    (trimmedValue.startsWith('a:') || trimmedValue.startsWith('O:')) &&
-    trimmedValue.endsWith('}') &&
-    trimmedValue.includes('{')
-  );
 }
 
 function getFaUserFromUser(user) {
@@ -69,10 +57,10 @@ function getFaUserFromUser(user) {
   // faUser.birthDate = string;
   // faUser.cleanSpeakId = UUID;
   // faUser.expiry = number;
-  if (metaKeyExists('first_name', user.meta)) faUser.firstName = getMetaValue('first_name', meta);
+  if (metaKeyExists('first_name', user.meta)) faUser.firstName = getMetaValue('first_name', user.meta);
   // faUser.imageUrl = string;
   // faUser.insertInstant = number;
-  if (metaKeyExists('last_name', user.meta)) faUser.lastName = getMetaValue('last_name', meta);
+  if (metaKeyExists('last_name', user.meta)) faUser.lastName = getMetaValue('last_name', user.meta);
   faUser.fullName = [faUser.firstName, faUser.lastName]
     .filter(name => name !== null && name !== undefined && name !== '')
     .join(' ');
@@ -80,35 +68,21 @@ function getFaUserFromUser(user) {
   // faUser.memberships = Array<GroupMember>;
   // faUser.middleName = string;
   // faUser.mobilePhone = string;
-  if (user.phone_numbers?.length > 0)
-    faUser.mobilePhone = user.phone_numbers[0].phone_number;
+  // faUser.mobilePhone = string;
   // faUser.parentEmail = string;
   // faUser.preferredLanguages = Array<string>;
   // faUser.registrations = Array<UserRegistration>;
   // faUser.tenantId = UUID;
   // faUser.timezone = string;
   // faUser.twoFactor = UserTwoFactorConfiguration;
-  faUser.data = {}
-  if (user.biometric_registrations?.length > 0)
-    faUser.data.stytch_biometric_registrations = user.biometric_registrations;
-  faUser.data.stytch_created_at = user.created_at;
-  if (user.crypto_wallets?.length > 0)
-    faUser.data.stytch_crypto_wallets = user.crypto_wallets;
-  if (user.emails?.length > 1)
-    faUser.data.stytch_emails = user.emails.filter(e => e.email != faUser.email);
-  if (user.phone_numbers?.length > 1)
-    faUser.data.stytch_phone_numbers = user.phone_numbers.filter(e => e.phone_number != faUser.mobilePhone);
-  if (user.providers?.length > 0)
-    faUser.data.stytch_providers = user.providers;
-  if (user.trusted_metadata && Object.keys(user.trusted_metadata).length > 0)
-    faUser.data.stytch_trusted_metadata = user.trusted_metadata;
-    if (user.untrusted_metadata && Object.keys(user.untrusted_metadata).length > 0)
-    faUser.data.stytch_untrusted_metadata = user.untrusted_metadata;
-  faUser.data.stytch_user_id = user.user_id;
-  if (user.webauthn_registrations?.length > 0)
-    faUser.data.stytch_webauthn_registrations = user.webauthn_registrations;
-  faUser.data.stytch_export_request_id = user.request_id;
 
+  faUser.data = {}
+  faUser.data.WordPress_ID = user.ID;
+  faUser.data.WordPress_user_nicename = user.user_nicename;
+  faUser.data.WordPress_user_url = user.user_url;
+  faUser.data.WordPress_user_registered = user.user_registered;
+  faUser.data.WordPress_display_name = user.display_name;
+  user.meta.map(pair => addMetaData(pair, faUser.data));
   return faUser;
 }
 
@@ -120,4 +94,22 @@ function metaKeyExists(key, meta) {
 
 function getMetaValue(key, meta) {
   return meta.find(pair => pair.meta_key === key).meta_value;
+}
+
+function addMetaData(pair, data) {
+  if (dataToIgnore.includes(pair.meta_key)) return;
+  data['WordPress_' + pair.meta_key] = pair.meta_value;
+  if (isSerializedPhp(pair.meta_value))
+    data['WordPress_' + pair.meta_key] = JSON.stringify(phpunserialize(pair.meta_value));
+}
+
+function isSerializedPhp(value) {
+  if (typeof value !== 'string')
+    return false;
+  const trimmedValue = value.trim();
+  return (
+    (trimmedValue.startsWith('a:') || trimmedValue.startsWith('O:')) &&
+    trimmedValue.endsWith('}') &&
+    trimmedValue.includes('{')
+  );
 }
