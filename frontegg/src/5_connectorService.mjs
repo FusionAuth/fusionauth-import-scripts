00ff9c9e088a68711cd7f24be4312e91899fb1a8
RichardJECooke <RichardJECooke@pm.me>
2024-03-20 16:13:26 +0200

    fixed connector service and can now log in

--- frontegg/src/5_connectorService.mjs
+++ frontegg/src/5_connectorService.mjs
@@ -4,14 +4,32 @@
 
 import express from 'express';
 import * as egg from '@frontegg/client';
+import * as fs from 'fs';
+import parser from 'stream-json';
+import StreamArray from 'stream-json/streamers/StreamArray.js';
+import Chain from 'stream-chain';
 
 const app = express();
 app.use(express.json());
 app.post('/', async (request, response) => {
     console.log("Request received");
-    const isValid = await isLoginValid(request.body.loginId, request.body.password);
-    if (isValid)
-        return response.status(200).json({});
+    const email = request.body.loginId;
+    const password = request.body.password;
+    const isValid = await isLoginValid(email, request.body.password);
+    if (isValid) {
+        const idAndActive = await getUserDetailsFromFile(email);
+        if (idAndActive.length != 2)
+            return response.status(404).end();
+        return response.status(200).json({
+            user: {
+                'active': idAndActive[1],
+                'id': idAndActive[0],
+                'email': email,
+                'password': password,
+                'username': email
+            }
+        });
+    }
     response.status(404).end();
 });
 app.listen(6252);
@@ -29,10 +47,22 @@ async function isLoginValid(id, password) {
         });
         return (response.status == 200 && response.data.valid);
     }
-    catch {
+    catch (e) {
         return false;
     }
     finally {
         authenticator.shutdown();
     }
+}
+
+async function getUserDetailsFromFile(email) {
+    const inputUsers = new Chain([fs.createReadStream('faUsers.json'), parser(), new StreamArray(),]);
+    const result = [];
+    for await (const { value: user } of inputUsers) {
+        if (user.email == email) {
+            result.push(user.id);
+            result.push(user.active);
+        }
+    }
+    return result;
 }
