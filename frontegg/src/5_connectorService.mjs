95e1871668e62716761b2fda7858cfcebe612251
RichardJECooke <RichardJECooke@pm.me>
2024-03-20 13:34:11 +0200

    fix servce

--- frontegg/src/5_connectorService.mjs
+++ frontegg/src/5_connectorService.mjs
@@ -8,10 +8,11 @@ import * as egg from '@frontegg/client';
 const app = express();
 app.use(express.json());
 app.post('/', async (request, response) => {
-    const isValid = await isLoginValid(request.body.loginId, password);
+    console.log(request.body.loginId + " request received");
+    const isValid = await isLoginValid(request.body.loginId, request.body.password);
     if (isValid)
         return response.status(200).json({});
-    response.status(404);
+    response.status(404).end();
 });
 app.listen(6252);
 
@@ -21,10 +22,17 @@ async function isLoginValid(id, password) {
     const authenticator = new egg.FronteggAuthenticator();
     await authenticator.init(process.argv[2], process.argv[3]);
     const httpClient = new egg.HttpClient(authenticator, { baseURL: 'https://api.frontegg.com' });
-    const response = await httpClient.post('identity/resources/vendor-only/users/v1/passwords/verify', {
-        email: id,
-        password: password
-    });
-    authenticator.shutdown();
-    return (response.status == 200 && response.data.valid) ;
+    try {
+        const response = await httpClient.post('identity/resources/vendor-only/users/v1/passwords/verify', {
+            email: id,
+            password: password
+        });
+        return (response.status == 200 && response.data.valid);
+    }
+    catch {
+        return false;
+    }
+    finally {
+        authenticator.shutdown();
+    }
 }
