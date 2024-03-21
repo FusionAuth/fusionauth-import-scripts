157e15c9ccc29ecdbf63d661055cc21ba7fbbdab
RichardJECooke <RichardJECooke@pm.me>
2024-03-21 12:55:38 +0200

    web app progress

--- generic/src/3_webApp.mjs
+++ generic/src/3_webApp.mjs
@@ -1,21 +1,24 @@
 import argon2 from 'argon2';
 import express from 'express';
 import session from 'express-session';
+import pg from "pg"
+
+const db = new pg.Pool({
+    user: 'p',
+    host: 'localhost',
+    database: 'p',
+    password: 'p',
+    port: 5432,
+});
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret: 'yourSecretKey',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false } // no https
 }));
-
-app.get('/account', async (request, response) => {
-    if (request.session.userEmail)
-        response.send(`<html><body><h1>Account Details</h1><p>Hello ${request.session.userEmail}</p></body></html>`);
-    else
-        response.send(`<html><body><h1>Access Denied</h1></body></html>`);
-});
 
 app.get('/', async (request, response) => {
     response.send(`
@@ -33,11 +36,30 @@ app.get('/', async (request, response) => {
     `);
 });
 
-
 app.post('/', async (request, response) => {
-    request.session.userEmail = 'email'
-    response.redirect('/account');
+    const { email, password } = request.body;
+    if (!email || !password)
+        return response.status(400).send('Enter email and password');
+    try {
+        const hash = await argon2.hash(password);
+        await db.query(
+            'INSERT INTO "user" (email, passwordhash) VALUES ($1, $2) RETURNING *;',
+            [email, hash]
+        );
+        request.session.userEmail = email;
+        response.redirect('/account');
+    }
+    catch (error) {
+        console.log(error);
+        response.status(500).send('Error saving user');
+    }
 });
 
+app.get('/account', async (request, response) => {
+    if (request.session.userEmail)
+        response.send(`<html><body><h1>Account Details</h1><p>Hello ${request.session.userEmail}</p></body></html>`);
+    else
+        response.send(`<html><body><h1>Access Denied</h1></body></html>`);
+});
 
 app.listen(7771);
