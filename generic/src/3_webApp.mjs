1f0d9622e534ddcdc7f246721b02c1f0bb9de319
RichardJECooke <RichardJECooke@pm.me>
2024-03-21 12:36:04 +0200

    created form

--- generic/src/3_webApp.mjs
+++ generic/src/3_webApp.mjs
@@ -14,15 +14,30 @@ app.get('/account', async (request, response) => {
     if (request.session.userEmail)
         response.send(`<html><body><h1>Account Details</h1><p>Hello ${request.session.userEmail}</p></body></html>`);
     else
-    response.send(`<html><body><h1>Access Denied</h1></body></html>`);
+        response.send(`<html><body><h1>Access Denied</h1></body></html>`);
 });
 
 app.get('/', async (request, response) => {
+    response.send(`
+        <html>
+            <body>
+                <form action="/" method="post">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required> <br /><br />
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required> <br /><br />
+                    <button type="submit">Sign In / Register</button>
+                </form>
+            </body>
+        </html>
+    `);
+});
+
+
+app.post('/', async (request, response) => {
     request.session.userEmail = 'email'
     response.redirect('/account');
 });
-
-app.post('/', async (request, response) => { });
 
 
 app.listen(7771);
