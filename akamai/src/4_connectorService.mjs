6bffdc79ed6577a7d182424fe5b732a338b4ff32
RichardJECooke <RichardJECooke@pm.me>
2025-05-14 21:47:31 +0200

    article draft done

--- akamai/src/4_connectorService.mjs
+++ akamai/src/4_connectorService.mjs
@@ -1,9 +1,6 @@
-// Usage: node 5_connectorService.mjs yourclientid yourapikey
-
 // Documentation at https://fusionauth.io/docs/lifecycle/migrate-users/connectors/generic-connector
 
 import express from 'express';
-import * as egg from '@frontegg/client';
 import * as fs from 'fs';
 import parser from 'stream-json';
 import StreamArray from 'stream-json/streamers/StreamArray.js';
@@ -12,53 +9,49 @@ import Chain from 'stream-chain';
 const app = express();
 app.use(express.json());
 app.post('/', async (request, response) => {
-    console.log("Request received");
+    console.log(`Request received for ${request.body.loginId}`);
     const email = request.body.loginId;
     const password = request.body.password;
     const isValid = await isLoginValid(email, request.body.password);
-    if (isValid) {
-        const userDetails = await getUserDetailsFromFile(email);
-        if (Object.keys(userDetails).length === 0)
-            return response.status(404).end();    
-        return response.status(200).json({
-            user: {
-                ...userDetails,
-                'password': password
-            }
-        });
-    }
-    response.status(404).end();
+    if (!isValid)
+      return response.status(404).end();
+    const userDetails = await getUserDetailsFromFile(email);
+    if (Object.keys(userDetails).length === 0)
+        return response.status(404).end();
+    console.log('User login verified. Returning details to FusionAuth');
+    return response.status(200).json({user: { ...userDetails, 'password': password }});
 });
-app.listen(6252);
+app.listen(80, '0.0.0.0');
+console.log('Service running');
 
-
-
-async function isLoginValid(id, password) {
-    const authenticator = new egg.FronteggAuthenticator();
-    await authenticator.init(process.argv[2], process.argv[3]);
-    const httpClient = new egg.HttpClient(authenticator, { baseURL: 'https://api.frontegg.com' });
+async function isLoginValid(email, password) {
     try {
-        const response = await httpClient.post('identity/resources/vendor-only/users/v1/passwords/verify', {
-            email: id,
-            password: password
-        });
-        return (response.status == 200 && response.data.valid);
+      const response = await fetch('https://test-env.us-dev.janraincapture.com/oauth/auth_native_traditional', {
+        method: 'POST',
+        headers: { 'accept': 'application/json', 'content-type': 'application/x-www-form-urlencoded'},
+        body: new URLSearchParams({
+          client_id: '9a55jnetfgjf8mzdmjbugdmyr55tc2j9',
+          flow_version: '20250502165159394037',
+          flow: 'standard',
+          locale: 'en-US',
+          form: 'signInForm',
+          redirect_uri: 'http://localhost',
+          currentPassword: password,
+          signInEmailAddress: email
+        }).toString()
+      });
+      const data = await response.json();
+      console.dir(data);
+      return (response.status == 200 && data.stat == 'ok');
     }
-    catch (e) {
-        return false;
-    }
-    finally {
-        authenticator.shutdown();
-    }
+    catch (e) { return false; }
 }
 
 async function getUserDetailsFromFile(email) {
-    const inputUsers = new Chain([fs.createReadStream('faUsers.json'), parser(), new StreamArray(),]);
-    let result = {};
-    for await (const { value: user } of inputUsers) {
-        if (user.email == email) {
-            result = user;
-        }
-    }
-    return result;
+  const inputUsers = new Chain([fs.createReadStream('faUsers.json'), parser(), new StreamArray(),]);
+  for await (const { value: user } of inputUsers)
+      if (user.email == email)
+        return user;
+  console.log(email + ' not found in faUsers.json');
+  return {};
 }
